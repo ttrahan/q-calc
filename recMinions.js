a5f25fc0d48ce780a31f8e6be7a4b2b713032f1b
Tom Trahan <tom@shippable.com>
2015-12-08 11:59:56 -0800

    update command line prompt

--- recMinions.js
+++ recMinions.js
@@ -17,22 +17,19 @@ const roundNum = require('./modules/roundNum-module.js');
 // 5: same as 4, but for 30 minute wait
 // 6: the duration of the peak period - if no peak, enter time for entire work shift, e.g. 8 hours
 
-const numDevPeak = process.argv[2]; // peak number of developers in organization working concurrently
+const numDevsPeak = process.argv[2]; // peak number of developers in organization working concurrently
 const numBuildsPerDevPeak = process.argv[3]; // avg number of builds per peak shift per developer
 const peakDurationHours = process.argv[4]; // duration in hours of peak period - if no peak, enter time for entire work shift, e.g. 8 hours
 const avgBuildDurationMin = process.argv[5]; // average build duration
 const s = 10000;  // max number of build containers to calculate Queue stats for
 const probAcceptable5MinWait = process.argv[6]; // acceptable probability of 5 minute wait time for build to start
 const probAcceptable30MinWait = process.argv[7]; // acceptable probability of 30 minute wait time for build to start
-const lambda = (numDevPeak * numBuildsPerDevPeak) / peakDurationHours ;  // arrival rate per hour
+const lambda = (numDevsPeak * numBuildsPerDevPeak) / peakDurationHours ;  // arrival rate per hour
 const mu = 60 / avgBuildDurationMin; // hourly service rate, i.e.
 
 // verify that valid inputs were provided as arguments
-if (! process.argv[2] || ! process.argv[3] || ! process.argv[4] || ! process.argv[5] ||
-   ! process.argv[6]) {
-     printResults('Parameters required for: \navgBuildDurationMin, numShifts, ' +
-      'maxNumberBuildContainersToEvaluate, AcceptableProb5MinWait, AcceptableProb30MinWait\n'+
-      'for example: $ node infiniteQueueCalc.js 50 5 20 .1 0'
+if (! process.argv[2] || ! process.argv[3] || ! process.argv[4] || ! process.argv[5] || ! process.argv[6] || ! process.argv[7]) {
+     printResults('Parameters required for: \nnumDevsPeak, numBuildsPerDevPeak, peakDurationHours, avgBuildDurationMin, AcceptableProb5MinWait, AcceptableProb30MinWait\n' + 'for example: $ node --harmony recMinions 50 1 2 20 .1 0'
     );
     return;
    }
